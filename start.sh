911ce95b3c2a6216b85cbbf249b23e6b7640c101
vip200 <51438486+vip200@users.noreply.github.com>
2023-07-31 00:18:57 +0300

    Add files via upload

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-gunicorn main:main --bind 0.0.0.0:8000 --timeout 86400 --worker-class aiohttp.GunicornWebWorker & python -m bot
+gunicorn main:main --bind 0.0.0.0:8080 --timeout 86400 --worker-class aiohttp.GunicornWebWorker & python -m bot
